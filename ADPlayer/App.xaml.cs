44ddb363d1006563bfb16963d1fba7a3e0041890
Alex-Dobrynin <shuron1988@gmail.com>
2019-08-08 18:30:42 +0300

    added solution architecture

--- ADPlayer/App.xaml.cs
+++ ADPlayer/App.xaml.cs
@@ -1,17 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+
+using ADPlayer.Views;
+
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
 
 namespace ADPlayer
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        public App()
+        {
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine(e.Exception.ToString());
+        }
+
+        protected override Window CreateShell()
+        {
+            var window = Container.Resolve<MainWindowView>();
+
+            return window;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            
+        }
     }
 }
